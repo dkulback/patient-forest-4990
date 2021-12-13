ec068a2d8d1fbafc86825fb3ab7df190c8d53d2b
Darren Kulback <dkulback@gmail.com>
2021-12-12 21:34:01 -0700

    add show functionality to movie

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -34,6 +34,23 @@ RSpec.describe 'movies show page' do
     visit "/movies/#{movie_1.id}"
     expected = [mike.name, harrison.name].join(" ")
     expect(page.find_all('.actors').map(&:text).join).to eq(expected)
-    expect(page).to have_content("Average age: #{Actor.average_age}")
+    within '.actor-age' do
+      expect(page).to have_content("Average age: #{Actor.average_age}")
+    end
+  end
+  it 'has a form to add actors to the movie already in DB' do
+    universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+    movie_1 = universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+    harrison = Actor.create!(name: 'Harrison Ford', age: 67)
+    mike = Actor.create!(name: 'Michael Myers', age: 63)
+    # As a user,
+    # When I visit a movie show page,
+    # I do not see any actors listed that are not part of the movie
+    # And I see a form to add an actor to this movie
+    # When I fill in the form with the name of an actor that exists in the database
+    # And I click submit
+    # Then I am redirected back to that movie's show page
+    # And I see the actor's name is now listed
+    # (You do not have to test for a sad path, for example if the name submitted is not an existing actor)
   end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe Actor, type: :model do
       mike = Actor.create!(name: 'Michael Myers', age: 63)
       expect(Actor.age_sort).to eq([mike, harrison])
     end
+    it 'lists actors average age' do
+
+      harrison = Actor.create!(name: 'Harrison Ford', age: 67)
+      mike = Actor.create!(name: 'Michael Myers', age: 63)
+      expect(Actor.average_age).to eq(65)
+    end
   end
 end
